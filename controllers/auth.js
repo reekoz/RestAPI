ad54963f86bb4b66e3ee04fd6f77b718b5906c42
Mattia Thiella <mattia.thiella@qualitas.it>
2021-05-19 19:37:47 +0200

    push all on github

---
+++ controllers/auth.js
@@ -0,0 +1,84 @@
+const User = require('../models/user');
+const {
+    validationResult
+} = require('express-validator');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+exports.signup = async (req, res, next) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+        const error = new Error('Validation failed');
+        error.statusCode = 422;
+        error.data = errors.array();
+        throw error;
+    }
+
+    const email = req.body.email;
+    const name = req.body.name;
+    const password = req.body.password;
+
+    try {
+        const hashedPw = await bcrypt.hash(password, 12);
+        const user = new User({
+            email: email,
+            password: hashedPw,
+            name: name
+        });
+        const result = await user.save();
+
+        res.status(201).json({
+            message: 'User created!',
+            userId: result._id
+        })
+    } catch (err) {
+        if (!err.statusCode) {
+            err.statusCode = 500;
+        }
+        next(err);
+    }
+};
+
+exports.login = async (req, res, next) => {
+    const email = req.body.email;
+    const password = req.body.password;
+    let loadedUser;
+
+    try {
+        const user = await User.findOne({
+            email: email
+        });
+
+        if (!user) {
+            const error = new Error('User not found');
+            error.statusCode = 404;
+            throw error;
+        }
+
+        loadedUser = user;
+        const isEqual = await bcrypt.compare(password, user.password);
+        if (!isEqual) {
+            const error = new Error('Wrong password');
+            error.statusCode = 401;
+            throw error;
+        }
+
+        const token = jwt.sign({
+            email: loadedUser.email,
+            userId: loadedUser._id.toString()
+        }, 'mysupersecretscretsecret', {
+            expiresIn: '1h'
+        });
+
+        res.status(200).json({
+            token: token,
+            userId: loadedUser._id.toString()
+        });
+    } catch (err) {
+        if (!err.statusCode) {
+            err.statusCode = 500;
+        }
+        next(err);
+    }
+};
---
+++ middleware/is-auth.js
@@ -0,0 +1,29 @@
+const jwt = require('jsonwebtoken');
+
+module.exports = (req, res, next) => {
+    const authHeader = req.get('Authentication');
+
+    if (!authHeader) {
+        const error = new Error('Not authenticated');
+        error.statusCode = 401;
+        throw error;
+    }
+
+    const token = authHeader.split(' ')[1];
+    let decodedToken;
+    try {
+        decodedToken = jwt.verify(token, 'mysupersecretscretsecret');
+    } catch (err) {
+        err.statusCode = 500;
+        throw err;
+    }
+
+    if (!decodedToken) {
+        const error = new Error('Not authenticated');
+        error.statusCode = 401;
+        throw error;
+    }
+
+    req.userId = decodedToken.userId;
+    next();
+};
