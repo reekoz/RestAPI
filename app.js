4d9cc1f62cf5ba6a9e16ea766348564d56948530
Mattia Thiella <mattia.thiella@qualitas.it>
2021-05-19 20:49:08 +0200

    start implementing Unsplash API

--- app.js
+++ app.js
@@ -1,37 +1,28 @@
 const express = require('express');
-const {
-    json
-} = require('body-parser');
+const { json } = require('body-parser');
 const mongoose = require('mongoose');
 const path = require('path');
 const multer = require('multer');
-const {
-    v4: uuidv4
-} = require('uuid');
-
+const { v4: uuidv4 } = require('uuid');
 const feedRoutes = require('./routes/feed');
 const authRoutes = require('./routes/auth');
-const {
-    mongo
-} = require('mongoose');
 const helmet = require('helmet');
 const compression = require('compression');
+
 const app = express();
 
 const fileStorage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, 'images');
-    },
-    filename: (req, file, cb) => {
-        cb(null, uuidv4() + '_' + file.originalname);
-    }
+  destination: (req, file, cb) => {
+    cb(null, 'images');
+  },
+  filename: (req, file, cb) => {
+    cb(null, uuidv4() + '_' + file.originalname);
+  },
 });
 
 const fileFilter = (req, file, cb) => {
-    if (file.mimetype.startsWith('image/'))
-        cb(null, true);
-    else
-        cb(null, false);
+  if (file.mimetype.startsWith('image/')) cb(null, true);
+  else cb(null, false);
 };
 
 app.use(helmet());
@@ -39,40 +30,48 @@ app.use(compression());
 
 app.use(json());
 
-app.use(multer({
+app.use(
+  multer({
     storage: fileStorage,
-    fileFilter: fileFilter
-}).single('image'));
+    fileFilter: fileFilter,
+  }).single('image')
+);
 
 app.use('/images', express.static(path.join(__dirname, 'images')));
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authentication');
-    next();
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, PUT, PATCH, DELETE'
+  );
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authentication');
+  next();
 });
 
 app.use('/feed', feedRoutes);
 app.use('/auth', authRoutes);
 
 app.use((error, req, res, next) => {
-    console.log(error);
-    const status = error.statusCode || 500;
-    const message = error.message;
-    const data = error.data;
-    res.status(status).json({
-        message: message,
-        data: data
-    });
+  console.log(error);
+  const status = error.statusCode || 500;
+  const message = error.message;
+  const data = error.data;
+  res.status(status).json({
+    message: message,
+    data: data,
+  });
 });
 
 mongoose
-    .connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_ADDRESS}/${process.env.MONGO_DEFAULT_DATABASE}`)
-    .then(result => {
-        const server = app.listen(process.env.PORT || 3000);
-        const io = require('./socket').init(server);
-        io.on('connection', socket => {
-            console.log('Client connected');
-        });
-    }).catch(err => console.log(err));
+  .connect(
+    `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_ADDRESS}/${process.env.MONGO_DEFAULT_DATABASE}`
+  )
+  .then(result => {
+    const server = app.listen(process.env.PORT || 3000);
+    const io = require('./socket').init(server);
+    io.on('connection', socket => {
+      console.log('Client connected');
+    });
+  })
+  .catch(err => console.log(err));
--- routes/feed.js
+++ routes/feed.js
@@ -1,7 +1,7 @@
 const express = require('express');
-const {
-    body
-} = require('express-validator/check');
+const { body } = require('express-validator/check');
+const { createApi } = require('unsplash-js');
+const nodeFetch = require('node-fetch');
 
 const feedController = require('../controllers/feed');
 const isAuth = require('../middleware/is-auth');
@@ -12,35 +12,37 @@ const router = express.Router();
 router.get('/posts', isAuth, feedController.getPosts);
 
 // POST /feed/post
-router.post('/post', isAuth, [
-    body('title')
-    .trim()
-    .isLength({
-        min: 5
+router.post(
+  '/post',
+  isAuth,
+  [
+    body('title').trim().isLength({
+      min: 5,
     }),
-    body('content')
-    .trim()
-    .isLength({
-        min: 5
-    })
-], feedController.createPost);
+    body('content').trim().isLength({
+      min: 5,
+    }),
+  ],
+  feedController.createPost
+);
 
 // GET /feed/post/:postId
 router.get('/post/:postId', isAuth, feedController.getPost);
 
 // PUT /feed/post/:postId
-router.put('/post/:postId', isAuth, [
-    body('title')
-    .trim()
-    .isLength({
-        min: 5
+router.put(
+  '/post/:postId',
+  isAuth,
+  [
+    body('title').trim().isLength({
+      min: 5,
     }),
-    body('content')
-    .trim()
-    .isLength({
-        min: 5
-    })
-], feedController.updatePost);
+    body('content').trim().isLength({
+      min: 5,
+    }),
+  ],
+  feedController.updatePost
+);
 
 // DELETE /feed/post/:postId
 router.delete('/post/:postId', isAuth, feedController.deletePost);
@@ -49,10 +51,41 @@ router.delete('/post/:postId', isAuth, feedController.deletePost);
 router.get('/status', isAuth, feedController.getStatus);
 
 // PUT /auth/status
-router.put('/status', isAuth, [
-    body('status')
-    .trim()
-    .notEmpty()
-], feedController.updateStatus);
+router.put(
+  '/status',
+  isAuth,
+  [body('status').trim().notEmpty()],
+  feedController.updateStatus
+);
+
+// GET /feed/photo
+router.get('/photo', isAuth, async (req, res, next) => {
+  try {
+    const unsplash = createApi({
+      accessKey: process.env.UNSPLASH_KEY,
+      fetch: nodeFetch
+    });
+
+    const result = await unsplash.search.getPhotos({
+      query: req.query.term
+    });
+
+    if (result.errors) {
+        const error = new Error(result.errors.join(', '));
+        error.statusCode = 500;
+        throw error;
+    }
+
+    res.status(200).json({
+      message: 'Fetched photo successfully',
+      photo: result.response
+    });
+  } catch (err) {
+    if (!err.statusCode) {
+      err.statusCode = 500;
+    }
+    next(err);
+  }
+});
 
 module.exports = router;
